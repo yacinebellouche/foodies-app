e47baac4ba9b5d84dab63fc0288d6a6f3001da9e
Bellouche Yacine <yacine.bellouche@etudiant.univ-brest.fr>
2025-02-18 23:55:22 +0100

    Added the meals page logic and components

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,24 @@
-export default function MealsPage(){
-    return (
-        <main>
-          <h1 style={{ color: "white", textAlign: "center" }}>
-            Meals Page
-          </h1>
-        </main>
-      );
-
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1 style={{ color: "white", textAlign: "center" }}>
+          Delicios meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself, It is easy and fun!{" "}
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
