9512e781d0ac4afecaf878945c604704e23f4f6e
Bellouche Yacine <yacine.bellouche@etudiant.univ-brest.fr>
2025-02-22 23:56:53 +0100

    Handled Submitting better UX

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,7 @@
 import classes from "./page.module.css";
 import ImagePicker from "@/components/meals/image-picker";
 import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 export default function ShareMealPage() {
   return (
     <>
@@ -41,7 +42,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker name="image" label="Your image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
